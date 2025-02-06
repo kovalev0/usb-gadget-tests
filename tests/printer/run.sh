08a1d8d33515398f664646f334eecac32365d2a8
Vasiliy Kovalev <kovalev@altlinux.org>
2025-02-07 02:32:51 +0300

    tests/printer: add the expected emulation output and run script

---
+++ tests/printer/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Get the absolute path of the script directory
+script_dir="$(dirname "$(readlink -e "$0")")"
+pushd "$script_dir" >/dev/null || exit 1
+
+executable="../../src/printer/printer"
+
+# Check if the executable exists and is runnable
+if [[ ! -x "$executable" ]]; then
+    echo "Error: $executable is missing or not executable."
+    exit 1
+fi
+
+# Run the keyboard test and save the output
+"$executable" &> result
+
+popd >/dev/null
