e608aed20b89d406c426895e52d2d6b14cc32253
Vasiliy Kovalev <kovalev@altlinux.org>
2025-03-18 13:18:29 +0300

    tests/printer: add usblp availability check

--- tests/printer/run.sh
+++ tests/printer/run.sh
@@ -12,6 +12,24 @@ if [[ ! -x "$executable" ]]; then
     exit 1
 fi
 
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
+# usblp built-in or already loaded ?
+if [[ ! -d "/sys/bus/usb/drivers/usblp" ]]; then
+    if modinfo usblp >/dev/null 2>&1; then
+        if modprobe usblp; then
+            sleep 1
+        else
+            echo -e "${YELLOW}Error: Failed to load usblp${NC}"
+            exit 70
+        fi
+    else
+            echo -e "${YELLOW}Warning: usblp module is not available (not built-in or loadable).${NC}"
+            exit 70
+    fi
+fi
+
 # Run the test and save the output
 "$executable" &> result
 
